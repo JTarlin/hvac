99cbd02178fea44c88c70cc89d0f28cd2a35f27e
Jackson Tarlin <jacksontarlin@gmail.com>
2021-01-28 15:21:20 -0800

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+//package depedencies
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+//component imports
+import Header from "./Components/Header/Header";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          {/* render header regardless of page we're on */}
+          <Header />
+          {/* otherwise content depends on route */}
+          {/* <Switch>
+            <Route exact path="/" render={()=> <About animStyle={animStyle} setAnimStyle={setAnimStyle}/>}  />
+            <Route exact path="/webdev" render={()=> <WebDev animStyle={animStyle} setAnimStyle={setAnimStyle}/>} />
+            <Route exact path="/gamedev" render={()=> <GameDev animStyle={animStyle} setAnimStyle={setAnimStyle}/>} />
+            <Route path="/webdev/lightspeed" render={()=> <Lightspeed animStyle={animStyle} setAnimStyle={setAnimStyle}/>} />
+            <Route path="/webdev/postal" render={()=> <Postal animStyle={animStyle} setAnimStyle={setAnimStyle}/>} />
+            <Route path="/gamedev/labyrinth" render={()=> <Labyrinth animStyle={animStyle} setAnimStyle={setAnimStyle}/>} />
+            <Route path="/gamedev/wordworlds" render={()=> <WordWorlds animStyle={animStyle} setAnimStyle={setAnimStyle}/>} />
+          </Switch>
+          // render footer regardless of what page we're on (sound familiar?)
+          <Footer /> */}
+        </Router>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+
+//import styling
+import "./Header.scss";
+
+export default function Header() {
+
+    return (
+        <header>
+            <div>
+                <div>hvac.ai</div>
+                
+            </div>
+        </header>
+    )
+}
