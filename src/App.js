74b2d41842606838172470003f9c5d27ae879a68
Jackson Tarlin <jacksontarlin@gmail.com>
2021-02-01 09:30:38 -0800

    added routes for footer, signin, dashboard

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 //component imports
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
+import SignIn from "./Components/SignIn/SignIn";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 import './App.css';
 
@@ -19,15 +21,11 @@ function App() {
           {/* otherwise content depends on route */}
           <Switch>
             <Route exact path="/"> <Home />  </Route>
-            {/* <Route exact path="/webdev" render={()=> <WebDev animStyle={animStyle} setAnimStyle={setAnimStyle}/>} />
-            <Route exact path="/gamedev" render={()=> <GameDev animStyle={animStyle} setAnimStyle={setAnimStyle}/>} />
-            <Route path="/webdev/lightspeed" render={()=> <Lightspeed animStyle={animStyle} setAnimStyle={setAnimStyle}/>} />
-            <Route path="/webdev/postal" render={()=> <Postal animStyle={animStyle} setAnimStyle={setAnimStyle}/>} />
-            <Route path="/gamedev/labyrinth" render={()=> <Labyrinth animStyle={animStyle} setAnimStyle={setAnimStyle}/>} />
-            <Route path="/gamedev/wordworlds" render={()=> <WordWorlds animStyle={animStyle} setAnimStyle={setAnimStyle}/>} /> */}
+            <Route exact path="/signin"> <SignIn /> </Route>
+            <Route exact path="/dashboard"> <Dashboard /> </Route>
           </Switch>
-          {/* // render footer regardless of what page we're on (sound familiar?)
-          <Footer /> */}
+          {/* render footer regardless of what page we're on (sound familiar?) */}
+          <Footer />
         </Router>
         </ParallaxProvider>
     </div>
